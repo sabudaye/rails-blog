a900aa5eeff452b0120185af9ebc1e716da79f0f
cafekoma <cafekoma@gmail.com>
2013-08-09 07:14:16 +0000

    trying to change coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,12 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
 
 if ENV['TRAVIS']
   require 'coveralls'
   Coveralls.wear!('rails')
 end
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
@@ -16,3 +17,4 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
