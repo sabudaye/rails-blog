465262c6d08ebbb356df67fbdca73c4947532a48
sabudaye <cafekoma@gmail.com>
2013-08-15 08:12:08 +0000

    remove many files

--- app/models/comment.rb
+++
@@ -1,7 +0,0 @@
-class Comment < ActiveRecord::Base
-  belongs_to :post
-  attr_accessible :body, :commenter
-
-  validates :body, :presence => true,
-                    :length => { :maximum => 1000  }
-end
--- app/models/tag.rb
+++
@@ -1,7 +0,0 @@
-class Tag < ActiveRecord::Base
-  belongs_to :post
-  attr_accessible :name
-
-  validates :name, :presence => true,
-            :length => { :minimum => 2, :maximum => 10  }
-end
